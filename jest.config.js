9c09fb41658b3fb8b5855a03dfafc274c1df8fb4
hadeer <hadeeressam21@gmail.com>
2025-10-31 23:58:16 +0200

    add packages and permissions for camera

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   preset: 'react-native',
   moduleNameMapper: {
     '\\.(ttf)$': '<rootDir>/__mocks__/file-mock.js',
-  }
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native|react-native.*|@react-native.*|@?react-navigation.*|@shopify/react-native-skia)/)"
+  ],
+  // Other values
+  testEnvironment: "@shopify/react-native-skia/jestEnv.js",
+  setupFilesAfterEnv: [
+    "@shopify/react-native-skia/jestSetup.js",
+  ],
 };
