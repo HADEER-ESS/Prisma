fe6f4d3422dacd998529c971ac81f317636ebb7f
hadeer <hadeeressam21@gmail.com>
2025-12-06 00:09:03 +0200

    create some component inside RESULT screen, where show the analysis from Google AI API

--- src/screens/ResultScreen.tsx
+++ src/screens/ResultScreen.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Text, View, Image } from 'react-native'
 
-const ResultScreen = () => {
+type Props = {
+    route: {
+        params: {
+            photoUri: string
+        }
+    }
+}
+
+const ResultScreen = ({ route }: Props) => {
+    const { photoUri } = route.params;
+
+    console.log('Received photo URI:', photoUri);
     return (
-        <></>
+        <View>
+            <Text> Welcome in Result screen </Text>
+            <Image
+                source={{ uri: photoUri }}
+                resizeMode='contain'
+                style={{ width: 300, height: 400 }}
+            />
+        </View>
     )
 }
 
